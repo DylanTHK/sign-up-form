67db3d09b737838a6a4df37173447230aeac35f2
DylanTHK <dylantanhk@gmail.com>
2022-06-26 09:55:49 +0800

    UPDATED CSS AND HTML
    - confirmed HTML layout index.html
    - updated CSS design in style.css
    - added selectors for app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+let submitButton = document.querySelector("#create-account");
+
+
+
